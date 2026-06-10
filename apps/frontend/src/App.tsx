c498f14e4460a6622578fde593ce40cfffc87c78
OmRana2004 <omrana8755800322@gmail.com>
2026-06-11 03:19:41 +0530

    From.tsx

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,39 +1,20 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { APITester } from "./APITester";
-import "./index.css";
-
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
+import "../styles/globals.css";
+import { Form } from "./components/Form";
+import { useState } from "react";
+import { Interview } from "./components/Interview";
+import { Result } from "./components/Result";
+import { Toaster } from "sonner";
 
 export function App() {
+  const [page, setPage] = useState<"form" | "interview" | "result">("form"); 
+
   return (
-    <div className="container mx-auto p-8 text-center relative z-10">
-      <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] [animation:spin_20s_linear_infinite]"
-        />
-      </div>
-      <Card>
-        <CardHeader className="gap-4">
-          <CardTitle className="text-3xl font-bold">Bun + React</CardTitle>
-          <CardDescription>
-            Edit <code className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono">src/App.tsx</code> and save to
-            test HMR
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <APITester />
-        </CardContent>
-      </Card>
+    <div className="flex h-screen w-screen items-center justify-center">
+      {page === "form" && <Form />}
+      {page === "interview" && <Interview />}
+      {page === "result" && <Result />}
+      <Toaster position="top-center" />
     </div>
   );
 }
-
 export default App;
---
+++ apps/frontend/src/components/Form.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { toast } from "sonner";
+import axios from "axios";
+import { BACKEND_URL } from "@/lib/config";
+
+export function Form() {
+  const [github, setGithub] = useState("");
+  const [linked, setLinked] = useState("");
+
+ async function onSubmit() {
+    if (!github || !linked) {
+      toast("Please fill in both fields");
+      return;
+    }
+
+    await axios.post(`${BACKEND_URL}/api/v1/pre-interview`,{
+        linked,
+        github
+    })
+  }
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+          Ai Interview Kickstart
+        </h2>
+        <div className="p-2">
+          <Input
+            placeholder="Linked URL"
+            onChange={(e) => setLinked(e.target.value)}
+          />
+        </div>
+        <div className="p-2">
+          <Input
+            placeholder="Github URL"
+            onChange={(e) => setGithub(e.target.value)}
+          />
+        </div>
+        <div className="flex justify-center p-4">
+          <Button onClick={onSubmit}>Start Interview</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
