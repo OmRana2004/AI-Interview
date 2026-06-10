175c8a4356bba3450533e8d91e27aebcba908a3e
OmRana2004 <omrana8755800322@gmail.com>
2026-06-11 03:45:50 +0530

    axios added

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import axios from "axios";
+import { PreInterviewBody } from "./types"
 
 const app = express ()
 app.use(express.json());
+
+app.post("/api/v1/pre-interview", (req, res) => {
+    const { success, data } = PreInterviewBody.safeParse(req.body);
+
+    if (!success) {
+        res.status(411).json({
+            message: "Incorrect body"
+        })
+    }
+})
 
 app.get("/", (req, res) => {
     res.send("Hello world!")
