7b8a1c1688b15aa8b4ef4b6c0b7a2c4690ad4602
OmRana2004 <omrana8755800322@gmail.com>
2026-06-11 03:30:44 +0530

    zod added

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express ()
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world!")
+})
+
+app.listen(3001, () =>{
+    console.log("port is listeing on port 3001")
+})
